ca7c713da12a18697f6738cee7a61183137ffd26
Lahatra Rajaonarison <lahatramaroo@gmail.com>
2026-02-17 15:46:47 +0300

    Implement slider functionality with navigation buttons

--- script.js
+++ script.js
@@ -15,3 +15,40 @@ window.addEventListener("scroll", () => {
   navBar.style.top = top;
   navBar.style.zIndex = 2000;
 });
+
+const swipperBtns = document.querySelectorAll(".slider-btn");
+const cards = document.querySelectorAll(".member-box");
+let index = 0;
+
+swipperBtns.forEach((s) => {
+  s.addEventListener("click", (e) => {
+    const parent = e.target.parentNode;
+    const action = parent.dataset.action;
+
+    switch (action) {
+      case "previous":
+        index--;
+
+        if (index < 0) {
+          index = cards.length - 1;
+        }
+        break;
+
+      case "next":
+        index++;
+
+        if (index > cards.length - 1) {
+          index = 0;
+        }
+        break;
+    }
+
+    for (let i = 0; i < cards.length; i++) {
+      const c = cards[i];
+
+      c.classList.remove("showed");
+    }
+
+    cards[index].classList.add("showed");
+  });
+});
